176e4e53b3c7eaa7aebcf28f6407a0d7cad858f1
Nathan Fritz <fritzy@github.com>
2024-09-26 01:07:28 -0700

    fix codespaces bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,13 @@ then
   fi
   mkdir -p $HOME/.local/opt
   tar -C $HOME/.local/opt -xzf nvim-linux64.tar.gz
+  rm $HOME/.local/bin/nvim
   ln -s $HOME/.local/opt/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
+  rm -rf $HOME/.config/nvim
+  mkdir -p $HOME/.config/nvim
+  cp -R ./.config/nvim/* $HOME/.config/nvim/
+  nvim --headless "+Lazy! sync" +qa
+  nvim --headless "+MasonInstall typescript-language-server eslint-lsp" +qa
 else
   echo "Not a Codespace, skipping neovim install."
 fi
