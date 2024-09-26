41cc32f300978067017e54309a1cf5c97748fe3b
Nathan Fritz <fritzy@github.com>
2024-09-26 00:28:04 -0700

    updated bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,15 @@
 
 if [ "$CODESPACES" == "true" ]
 then
-  echo "installing neovim"
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-  sudo tar -C $HOME/opt -xzf nvim-linux64.tar.gz
-  ln -s $HOME/opt/nvim-linux64/bin/nvim $HOME/bin/nvim
+  echo "Installing neovim..."
+  if [ ! -f nvim-linux64.tar.gz ]; then
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+  else
+    echo "Already downloaded tgz"
+  fi
+  mkdir -p $HOME/.local/opt
+  tar -C $HOME/.local/opt -xzf nvim-linux64.tar.gz
+  ln -s $HOME/.local/opt/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
 else
-  echo -e "Not a Codspace, skipping neovim install."
+  echo "Not a Codespace, skipping neovim install."
 fi
